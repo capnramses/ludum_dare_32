342159dd21f487b1fbe1ea23cc2b345c88b67166
capnramses <lists@antongerdelan.net>
2015-04-18 10:38:31 +0100

    moved old stuff

---
+++ starter_code/backdrop.js
@@ -0,0 +1,22 @@
+//
+// thinking:
+// stitch together several fixed-sizes images to form a level's backdrop
+// 1024x256 per piece?
+// this should make it easy to know which pieces to draw for the current scene
+//
+
+var backdrop_images = [
+	"textures/bd0.png",
+	"textures/bd1.png"
+];
+
+function init_backdrop () {
+
+}
+
+//
+// param shd probably be current world pos of centre of view?
+//
+function draw_backdrop () {
+
+}
